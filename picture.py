0267bae8f71a7f9573612087f25f791e20a8b101
Terminika <veronikadorohovitch@yandex.ru>
2021-09-22 23:03:15 +1000

    bugs fixed

--- picture.py
+++ picture.py
@@ -7,13 +7,16 @@ rect(screen, '#30ba8f', (0, 400, 1200, 800))
 # boy
 ellipse(screen, '#baacc7', (330, 250, 140, 350))
 circle(screen, '#dcdcdc', (400, 200), 70)
+# hands
 line(screen, 'black', (360, 280), (280, 450))
 line(screen, 'black', (440, 280), (600, 450))
+# legs
 line(screen, 'black', (360, 570), (300, 730))
 line(screen, 'black', (300, 730), (270, 730))
 line(screen, 'black', (440, 570), (500, 730))
 line(screen, 'black', (500, 730), (530, 730))
 
+# ice cream
 polygon(screen, 'yellow', [(290, 460), (250, 380), (200, 420)])
 circle(screen, 'red', (225, 380), 20)
 circle(screen, 'brown', (200, 400), 20)
@@ -22,23 +25,21 @@ circle(screen, 'white', (190, 370), 20)
 # girl
 polygon(screen, '#cf59b1', [(800, 230), (710, 600), (890, 600)])
 circle(screen, '#dcdcdc', (800, 200), 70)
+# hands
 line(screen, 'black', (790, 280), (600, 450))
 line(screen, 'black', (810, 280), (920, 380))
 line(screen, 'black', (920, 380), (1010, 280))
-
+# legs
+line(screen, 'black', (760, 600), (680, 730))
+line(screen, 'black', (680, 730), (660, 730))
+line(screen, 'black', (840, 600), (920, 730))
+line(screen, 'black', (920, 730), (940, 730))
 
 # heart
 line(screen, 'black', (1000, 320), (1060, 200))
 polygon(screen, 'red', [(1060, 200), (1100, 150), (1060, 145)])
 circle(screen, 'red', (1070, 145), 15)
 circle(screen, 'red', (1090, 145), 15)
-
-
-line(screen, 'black', (760, 600), (680, 730))
-line(screen, 'black', (680, 730), (660, 730))
-line(screen, 'black', (840, 600), (920, 730))
-line(screen, 'black', (920, 730), (940, 730))
-
 
 
 FPS = 30
--- picture_2.py
+++ picture_2.py
@@ -5,14 +5,16 @@ screen = pygame.display.set_mode((1600, 800))
 rect(screen, '#afdafc', (0, 0, 1600, 400))
 rect(screen, '#30ba8f', (0, 400, 1600, 800))
 # boy
-ellipse(screen, '#baacc7', (330 - 200, 250, 140, 350))
-circle(screen, '#dcdcdc', (400 - 200, 200), 70)
-line(screen, 'black', (360 - 200, 280), (280 - 200, 450))
-line(screen, 'black', (440 - 200, 280), (600 - 200, 450))
-line(screen, 'black', (360 - 200, 570), (300 - 200, 730))
-line(screen, 'black', (300 - 200, 730), (270 - 200, 730))
-line(screen, 'black', (440 - 200, 570), (500 - 200, 730))
-line(screen, 'black', (500 - 200, 730), (530 - 200, 730))
+ellipse(screen, '#baacc7', (130, 250, 140, 350))
+circle(screen, '#dcdcdc', (200, 200), 70)
+# hands
+line(screen, 'black', (160, 280), (80, 450))
+line(screen, 'black', (240, 280), (400, 450))
+# legs
+line(screen, 'black', (160, 570), (100, 730))
+line(screen, 'black', (100, 730), (70, 730))
+line(screen, 'black', (240, 570), (300, 730))
+line(screen, 'black', (300, 730), (330, 730))
 
 # heart
 line(screen, 'black', (80, 460), (80, 200))
@@ -21,29 +23,31 @@ circle(screen, 'red', (60, 145), 25)
 circle(screen, 'red', (100, 145), 25)
 
 # girl
-polygon(screen, '#cf59b1', [(800 - 200, 230), (710 - 200, 600), (890 - 200, 600)])
-circle(screen, '#dcdcdc', (800 - 200, 200), 70)
-line(screen, 'black', (790 - 200, 280), (600 - 200, 450))
-line(screen, 'black', (810 - 200, 280), (910 - 200, 380))
-line(screen, 'black', (910 - 200, 380), (1000 - 200, 280))
-
-line(screen, 'black', (760 - 200, 600), (680 - 200, 730))
-line(screen, 'black', (680 - 200, 730), (660 - 200, 730))
-line(screen, 'black', (840 - 200, 600), (920 - 200, 730))
-line(screen, 'black', (920 - 200, 730), (940 - 200, 730))
+polygon(screen, '#cf59b1', [(600, 230), (510, 600), (690, 600)])
+circle(screen, '#dcdcdc', (600, 200), 70)
+# hands
+line(screen, 'black', (590, 280), (400, 450))
+line(screen, 'black', (610, 280), (710, 380))
+line(screen, 'black', (710, 380), (800, 280))
+# legs
+line(screen, 'black', (560, 600), (480, 730))
+line(screen, 'black', (480, 730), (460, 730))
+line(screen, 'black', (640, 600), (720, 730))
+line(screen, 'black', (720, 730), (740, 730))
 
 
 # girl2
 polygon(screen, '#cf59b1', [(1000, 230), (910, 600), (1090, 600)])
 circle(screen, '#dcdcdc', (1000, 200), 70)
+# hands
 line(screen, 'black', (1010, 280), (1200, 450))
 line(screen, 'black', (890, 380), (990, 280))
 line(screen, 'black', (800, 280), (890, 380))
-
-line(screen, 'black', (760 + 200, 600), (680 + 200, 730))
-line(screen, 'black', (680 + 200, 730), (660 + 200, 730))
-line(screen, 'black', (840 + 200, 600), (920 + 200, 730))
-line(screen, 'black', (920 + 200, 730), (940 + 200, 730))
+# legs
+line(screen, 'black', (960, 600), (880, 730))
+line(screen, 'black', (880, 730), (860, 730))
+line(screen, 'black', (1040, 600), (1120, 730))
+line(screen, 'black', (1120, 730), (1140, 730))
 
 # ice cream
 line(screen, 'black', (800, 300), (800, 200))
@@ -56,12 +60,14 @@ circle(screen, 'white', (800, 300 - 250), 25)
 # boy2
 ellipse(screen, '#baacc7', (1270, 250, 140, 350))
 circle(screen, '#dcdcdc', (1340, 200), 70)
+# hands
 line(screen, 'black', (1380, 280), (1450, 450))
 line(screen, 'black', (1300, 280), (1200, 450))
-line(screen, 'black', (360 + 940, 570), (300 + 940, 730))
-line(screen, 'black', (300 + 940, 730), (270 + 940, 730))
-line(screen, 'black', (440 + 940, 570), (500 + 940, 730))
-line(screen, 'black', (500 + 940, 730), (530 + 940, 730))
+# legs
+line(screen, 'black', (1300, 570), (1240, 730))
+line(screen, 'black', (1240, 730), (1210, 730))
+line(screen, 'black', (1380, 570), (1440, 730))
+line(screen, 'black', (1440, 730), (1470, 730))
 
 # ice cream
 line(screen, 'black', (1450, 450), (1500, 300))
