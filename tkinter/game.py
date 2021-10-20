82a3710c5f19497da526e46c9312900005e581af
Terminika <veronikadorohovitch@yandex.ru>
2021-10-20 16:23:26 +0300

    final

--- tkinter/game.py
+++ tkinter/game.py
@@ -1,10 +1,8 @@
 from tkinter import *
 from random import randrange as rnd, choice
-import time
 
 root = Tk()
 root.geometry('800x625')
-
 canv = Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
 score = 0
@@ -12,6 +10,7 @@ objects = []
 canv.pack(fill=BOTH, expand=1)
 l = Label(root, bg='black', fg='white', text=f'Score:{score}', width=20)
 l.pack()
+name = None
 
 
 colors = ['red', 'orange', 'yellow', 'blue', 'green', 'black']
@@ -22,7 +21,10 @@ def main():
     Главная функция программы. Запускает процесс игры.
     :return:
     """
-    all_balls()
+    new_name()
+    if name:
+        canv.delete(ALL)
+        all_balls()
 
 
 def all_balls():
@@ -41,7 +43,10 @@ def all_balls():
             x, y, direction = res[0], res[1], res[2]
             objects.pop(i)
             objects.insert(i, (x, y, r, direction, color))
-            canv.create_oval(x - r, y - r, x + r, y + r, fill=color)
+            if color == 'black':
+                draw_bomb(x, y, r, color)
+            else:
+                canv.create_oval(x - r, y - r, x + r, y + r, fill=color)
     root.after(50, all_balls)
 
 
@@ -112,6 +117,17 @@ def move(x, y, r, direction):
     return x, y, direction
 
 
+def draw_bomb(x, y, r, color):
+    canv.create_oval(x - r, y - r, x + r, y + r, fill=color)
+    canv.create_rectangle(x + r // 2, y - r, x + r // 2 + 20, y - r + 20, fill='red')
+    canv.create_line(x + r // 2 + 20, y - r, x + r, y - r - 20, x + r + 20, y - r, smooth=1)
+
+    # img = ImageTk.PhotoImage(Image.open('bomb.png'))
+    # panel = Label(root, image=img, width=81, height=8).place(x=x, y=y, width=81, height=83)
+    # panel.pack()
+    # canv.image.gird(row=0, column=0)
+
+
 def click(event):
     """
     обрабаывает событие клика мышки.
@@ -120,24 +136,56 @@ def click(event):
     :param event: событие клика мышки
     :return:
     """
-    global score, objects
+    global score, objects, name
     x1, y1 = event.x, event.y
     i = 0
     while i < len(objects):
         x, y, r, color = objects[i][0], objects[i][1], objects[i][2], objects[i][4]
         if (x1 - x) ** 2 + (y1 - y) ** 2 <= r ** 2:
             if color == 'black':
+                canv.delete(ALL)
+                save_results(score)
                 score = 0
+                name = None
                 objects = []
                 break
             else:
                 score += 1
                 objects.pop(i)
                 i += 1
-                print(x1, y1, score)
         i += 1
     l.config(text=f'Score:{score}')
     l.pack()
+
+
+def new_name():
+    global name
+
+    def set_name():
+        global name
+        name = message.get()
+        print(name, score)
+
+    message = StringVar()
+    a = Toplevel()
+    a.geometry('200x150')
+    a['bg'] = 'grey'
+    a.overrideredirect(True)
+    Label(a, text='Введите имя игрока') \
+        .pack(expand=1)
+    message_entry = Entry(canv, textvariable=message)
+    message_entry.place(relx=.5, rely=.1, anchor="c")
+    message_button = Button(text="Задать имя", command=set_name)
+    message_button.place(relx=.5, rely=.5, anchor="c")
+    a.destroy()
+    #canv.bind('<Button-2>', set_name)
+
+
+
+def save_results(numb):
+    with open('results.txt', 'a', encoding='utf-8') as f:
+        f.write(name + '  ' + str(numb) + '\n')
+    new_name()
 
 
 main()
